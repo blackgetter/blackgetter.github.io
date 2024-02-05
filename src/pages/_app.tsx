a9d21f87087e903764fc7e01dd5cc2d5a911802d
Suengkwon Yu <blackgetter62@gmail.com>
2024-02-06 05:38:47 +0900

    이미지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,21 @@ import type { AppProps } from "next/app";
  * @description SEO를 위해 본인의 정보로 수정해주세요.
  */
 const DEFAULT_SEO = {
-  title: "홍길동 | Front-End Dev",
-  description: "안녕하세요, 프론트엔드 개발자 홍길동입니다.",
-  canonical: "https://www.naver.com/",
+  title: "유승권 | Embedded Dev",
+  description: "안녕하세요, 임베디드 개발자 홍길동입니다.",
+  canonical: "https://blackgetter.github.io/resume/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
-    url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    url: "https://blackgetter.github.io/resume/",
+    title: "유승권 | Embedded Dev",
+    site_name: "유승권 | Embedded Dev",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "홍길동 | Front-End Dev",
+        alt: "유승권 | Embedded Dev",
       },
     ],
   },
@@ -34,11 +34,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "홍길동 | Front-End Dev",
+      content: "유승권 | Embedded Dev",
     },
     {
       name: "msapplication-tooltip",
-      content: "홍길동 | Front-End Dev",
+      content: "유승권 | Embedded Dev",
     },
     {
       name: "viewport",
